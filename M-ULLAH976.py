063eb3c536d91186782cc5fca3cee2f82ea7511e
Pakhtoon1 <108829433+Pakhtoon1@users.noreply.github.com>
2022-07-13 02:58:12 +0500

    Update M-ULLAH976.py

--- M-ULLAH976.py
+++ M-ULLAH976.py
@@ -188,7 +188,7 @@ def main_apv():
 
         
 
-    r1=requests.get("https://github.com/Pakhtoon1/APRVL2.txt").text
+    r1=requests.get("https://github.com/Pakhtoon1/approvel.txt").text
 
     if key1 in r1:
 
